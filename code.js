96b1bab495a8c19931d2e8215739281ce1b9295d
Shaun-Shane <1953181@tongji.edu.cn>
2021-04-28 17:26:52 +0800

    完成搜索过程演示

--- code.js
+++ code.js
@@ -2,13 +2,13 @@ const MAX_HEIGHT = 6;
 const MAX_WIDTH = 7;
 const EMPTY = 2;
 const INF = 1000000;
-const SEARCH_DEPTH = 2;
+var SEARCH_DEPTH = 4;
 
 var sdPlayer = 0; // 0 - yellow 1 - red
 var mode = 0; // 0 - alphaBeta，1 - minmax
 
-var display = true; // 是否演示搜索过程
 var gameOver = false;
+var stopDisplay = true; // 是否演示搜索过程
 
 var moveList = [];
 
@@ -39,26 +39,38 @@ var recordMove = (type, depth, column, alpha, beta) => {
 
 // 演示
 var displayMove = async (mvBest) => {
+    console.log(moveList.length);
     (function Loop(i) {
         setTimeout(() => {
             if (moveList[i].type == 0) {
-                setTimeout(drop(moveList[i].column), 200);
+                if (!stopDisplay) setTimeout(drop(moveList[i].column), 160);
             } else {
-                setTimeout(undoDrop(moveList[i].column), 200);
+                if (!stopDisplay) setTimeout(undoDrop(moveList[i].column), 160);
             }
-            if ((++i) < moveList.length) Loop(i);
-            else {
-                drop(mvBest).then(() => {
-                    gameOver = check();
-                    setTimeout(() => {
-                        if (gameOver) {
-                            if (sdPlayer == 1) alert("Yellow win");
-                            else alert("Red win");
+            if ((++i) >= moveList.length || stopDisplay) {
+                if (stopDisplay) {
+                    for (let j = i - 1; j < moveList.length; j++) {
+                        let column = moveList[j].column;
+                        if (moveList[j].type == 0) makeMove(column);
+                        else {
+                            document.getElementById("color" + ((7 - height[column]) * 10 + column)).setAttribute("class", "");
+                            undoMakeMove(column);
                         }
-                    }, 200);
-                });
-            }
-        }, 600);
+                    }
+                }
+
+                drop(mvBest);
+                gameOver = check();
+                console.log(gameOver);
+                setTimeout(() => {
+                    if (gameOver) {
+                        if (sdPlayer == 1) alert("Yellow win");
+                        else alert("Red win");
+                    }
+                }, 200);
+                return;
+            } else Loop(i);
+        }, 400);
     })(0);
 }
 
@@ -123,7 +135,7 @@ var check = () => { // 判断是否连成四子
     return false;
 }
 
-var drop = async (column) => { // 在第 column 落子 并演示动画
+var drop = (column) => { // 在第 column 落子 并演示动画
     let color = sdPlayer ? "Red" : "Yellow";
     if (height[column] == MAX_HEIGHT) return false;
     let currentHeight = MAX_HEIGHT;
@@ -135,20 +147,19 @@ var drop = async (column) => { // 在第 column 落子 并演示动画
                 document.getElementById("color" + ((6 - currentHeight) * 10 + column)).setAttribute("class", "");
             }
 
-            if (currentHeight > height[column] + 1) {
+            if (currentHeight > height[column]) {
                 currentHeight--;
                 Loop();
-            } else {
-                height[column]++;
-                table[height[column]][column] = sdPlayer;
-                changeSide(); // 交换走子方
             }
         }, 20);
     })();
+    height[column]++;
+    table[height[column]][column] = sdPlayer;
+    changeSide(); // 交换走子方
     return true;
 }
 
-var undoDrop = async (column) => { // 在第 column 取消落子 并演示动画
+var undoDrop = (column) => { // 在第 column 取消落子 并演示动画
     changeSide(); // 交换走子方
 
     let color = sdPlayer ? "Red" : "Yellow";
@@ -164,12 +175,11 @@ var undoDrop = async (column) => { // 在第 column 取消落子 并演示动画
             if (currentHeight < MAX_HEIGHT) {
                 currentHeight++;
                 Loop();
-            } else {
-                table[height[column]][column] = EMPTY;
-                height[column]--;
             }
         }, 20);
     })();
+    table[height[column]][column] = EMPTY;
+    height[column]--;
 }
 
 var makeMove = (column) => {
@@ -193,8 +203,11 @@ var evaluate = () => {
             for (let i = 0; i < 8; i++) {
                 if (isYellow(row + delta[i][0], column + delta[i][1])
                     && isYellow(row + 2 * delta[i][0], column + 2 * delta[i][1])
-                    && isEmpty(row + 3 * delta[i][0], column + 3 * delta[i][1]))
+                    && isEmpty(row + 3 * delta[i][0], column + 3 * delta[i][1])) {
                         vlPlayer += 720;
+                        if (isEmpty(row - delta[i][0], column - delta[i][1]))
+                            vlPlayer += 10000;
+                    }
                 if (isYellow(row + delta[i][0], column + delta[i][1])
                     && isEmpty(row + 2 * delta[i][0], column + 2 * delta[i][1])
                     && isYellow(row + 3 * delta[i][0], column + 3 * delta[i][1]))
@@ -223,8 +236,11 @@ var evaluate = () => {
             for (let i = 0; i < 8; i++) {
                 if (isRed(row + delta[i][0], column + delta[i][1])
                     && isRed(row + 2 * delta[i][0], column + 2 * delta[i][1])
-                    && isEmpty(row + 3 * delta[i][0], column + 3 * delta[i][1]))
+                    && isEmpty(row + 3 * delta[i][0], column + 3 * delta[i][1])) {
                         vlAI += 720;
+                        if (isEmpty(row - delta[i][0], column - delta[i][1]))
+                            vlAI += 10000;
+                    }
                 if (isRed(row + delta[i][0], column + delta[i][1])
                     && isEmpty(row + 2 * delta[i][0], column + 2 * delta[i][1])
                     && isRed(row + 3 * delta[i][0], column + 3 * delta[i][1]))
@@ -266,12 +282,12 @@ var alphaBeta = (depth, alpha, beta) => {
         if (height[column] == MAX_HEIGHT) continue;
 
         makeMove(column);
-        recordMove(0, depth, column, alpha, beta);
+        if (!stopDisplay) recordMove(0, depth, column, alpha, beta);
 
         vl = -alphaBeta(depth - 1, -beta, -alpha);
 
         undoMakeMove(column);
-        recordMove(1, depth, column, alpha, beta);
+        if (!stopDisplay) recordMove(1, depth, column, alpha, beta);
 
         if (vl > vlBest) {
             vlBest = vl, mvBest = column;
@@ -289,12 +305,12 @@ var searchRoot = (depth) => {
         if (height[column] == MAX_HEIGHT) continue;
 
         makeMove(column);
-        recordMove(0, depth, column, -INF, INF);
+        if (!stopDisplay) recordMove(0, depth, column, -INF, INF);
 
         vl = -alphaBeta(depth - 1, -INF, -vlBest);
 
         undoMakeMove(column);
-        recordMove(1, depth, column, -INF, INF);
+        if (!stopDisplay) recordMove(1, depth, column, -INF, INF);
 
         if (vl > vlBest) {
             vlBest = vl, mvBest = column;
@@ -312,28 +328,28 @@ var searchMain = () => {
 
     console.log(vlBest);
 
-    if (display) displayMove(mvBest);
+    if (!stopDisplay) displayMove(mvBest);
     else {
-        drop(mvBest).then(() => {
-            gameOver = check();
-            setTimeout(() => {
-                if (gameOver) {
-                    if (sdPlayer == 1) alert("Yellow win");
-                    else alert("Red win");
-                }
-            }, 200);
-        });
+        drop(mvBest);
+        gameOver = check();
+        setTimeout(() => {
+            if (gameOver) {
+                if (sdPlayer == 1) alert("Yellow win");
+                else alert("Red win");
+            }
+        }, 200);
     }
 }
 
 Array.from(document.getElementsByTagName("td")).forEach(item => {
-    item.onclick = async () => {
+    item.onclick = () => {
         if (gameOver) return;
         if(sdPlayer) return;
-        drop(getColumn(item.getAttribute("id"))).then((res) => {
-            if (!res) alert("Full");
-        }).then(() => {
+        let res = drop(getColumn(item.getAttribute("id")));
+        if (!res) alert("Full");
+        else {
             gameOver = check(); // 判断是否连成 4 子
+            console.log(gameOver);
             setTimeout(() => {
                 if (gameOver) {
                     if (sdPlayer == 1) alert("Yellow win");
@@ -342,12 +358,16 @@ Array.from(document.getElementsByTagName("td")).forEach(item => {
                     searchMain();
                 }
             }, 200);
-        })
+        }
     }
 });
 
 document.getElementById("restart").addEventListener('click', () => {
-    gameOver = false, display = false, mode = 0, sdPlayer = 0;
+    gameOver = false, mode = 0, sdPlayer = 0;
+    
+    stopDisplay = true;
+    document.getElementById("stop-display").innerHTML = "Choose display search";
+    
     setPlayerMessage();
     for (let row = 1; row <= MAX_HEIGHT; row++) 
         for (let column = 1; column <= MAX_WIDTH; column++) {
@@ -356,6 +376,13 @@ document.getElementById("restart").addEventListener('click', () => {
             document.getElementById("color" + (row * 10 + column)).setAttribute("class", "");
         }
     console.log(table);
-})
+});
+
+// 选择是否演示搜索过程。选择后可不进行演示或停止演示过程
+document.getElementById("stop-display").addEventListener('click', () => {
+    stopDisplay ^= 1;
+    if (stopDisplay) document.getElementById("stop-display").innerHTML = "Choose display search";
+    else document.getElementById("stop-display").innerHTML = "Stop display search";
+});
 
 setPlayerMessage();
